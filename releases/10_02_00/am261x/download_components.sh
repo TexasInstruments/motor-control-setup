eb609eb91b6d4b185dcd37cba3db2139a4850499
Dhaval Khandla <dhavaljk@ti.com>
2025-06-05 12:30:14 +0530

    am26xx: infra: Update MCUSDK links and component versions
    Fixes: PINDSW-9166
    
    Signed-off-by: Dhaval Khandla <dhavaljk@ti.com>

--- releases/10_02_00/am261x/download_components.sh
+++ releases/10_02_00/am261x/download_components.sh
@@ -73,7 +73,7 @@ mcu_plus_sdk_url="${MCU_PLUS_SDK_AM261X_RELEASE_URL}"
 mcu_sdk_version="${MCU_PLUS_SDK_AM261X}"
 
 # IND COMMS SDK
-ind_comms_sdk_url="${IND_COMMS_SDK_AM243X_NIGHTLY_URL}"
+ind_comms_sdk_url="${IND_COMMS_SDK_AM261X_RC_URL}"
 ind_comms_sdk_version="${IND_COMMS_SDK_AM261X}"
 
 platform="am261x"
